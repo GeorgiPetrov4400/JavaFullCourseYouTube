dcf0c2ed99d8eb68c2f31667590e6d378b730a6e
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-06-27 15:57:10 +0300

    Add files via upload

---
+++ Overloaded Constructor/Main.java
@@ -0,0 +1,14 @@
+package JavaFullCourseYoutube.OverloadedConstructors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pizza pizza = new Pizza("crust", "tomato", "mozzarella","pepperoni");
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+    }
+}
