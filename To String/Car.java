6dc177ed1c0ab11c671db120ba91e1a87f8b66d4
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-06-27 16:00:05 +0300

    Add files via upload

---
+++ To String/Car.java
@@ -0,0 +1,15 @@
+package JavaFullCourseYoutube.ToString;
+
+public class Car {
+
+    String make = "Lamborghini";
+    String model = "Murcielago";
+    String color = "yellow";
+    int year = 2021;
+
+    public String toString() {
+        /*String myString = make + "\n" + model + "\n" + color + "\n" + year;
+        return myString;*/
+        return make + "\n" + model + "\n" + color + "\n" + year;
+    }
+}
---
+++ To String/Main.java
@@ -0,0 +1,12 @@
+package JavaFullCourseYoutube.ToString;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        System.out.println(car.toString());
+        System.out.println();
+        System.out.println(car);
+    }
+}
