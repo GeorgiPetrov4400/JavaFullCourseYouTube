61f3f40c6739974b1212061c4cfa3f41ca65d806
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-06-27 15:50:04 +0300

    Add files via upload

---
+++ Abstraction/Car.java
@@ -0,0 +1,9 @@
+package JavaFullCourseYoutube.Abstraction;
+
+public class Car extends Vehicle {
+
+    @Override
+    void go() {
+        System.out.println("The driver is driving a car");
+    }
+}
