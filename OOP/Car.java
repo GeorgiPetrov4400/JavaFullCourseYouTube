3da5e62999f8d3b09b4a63ee6503ac3ae4731a3b
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-06-27 15:56:25 +0300

    Add files via upload

---
+++ OOP/Car.java
@@ -0,0 +1,17 @@
+package JavaFullCourseYoutube.OOP;
+
+public class Car {
+    String make = "Lamborghini";
+    String model = "Murcielago";
+    int year = 2020;
+    String color = "yellow";
+    double price = 200000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+
+    void brake() {
+        System.out.println("You step on the brakes");
+    }
+}
